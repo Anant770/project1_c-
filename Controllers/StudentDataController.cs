edf6570c0da7c24f812c69a024d867071b5e6dd7
Anant Chauhan <anantchauhan003@gmail.com>
2024-03-22 21:20:54 -0400

    updated

--- Controllers/StudentDataController.cs
+++ Controllers/StudentDataController.cs
@@ -18,9 +18,9 @@ namespace project1_c_.Controllers
         /// <summary>
         /// Returns a list of teachers in the system
         /// </summary>
-        /// <example>GET api/TeacherData/ListTeacher</example>
+        /// <example>GET api/StudentData/ListStudents</example>
         /// <returns>
-        /// A list of teacher
+        /// A list of students
         /// </returns>
         [HttpGet]
         [Route("api/StudentData/ListStudents/{SearchKey?}")]
@@ -43,11 +43,11 @@ namespace project1_c_.Controllers
 
             while (ResultSet.Read())
             {
-                int StudentId = (int)ResultSet["studentid"];
-                string StudentFname = (string)ResultSet["studentfname"];
-                string StudentLname = (string)ResultSet["studentlname"];
-                string StudentNumber = (string)ResultSet["studentnumber"];
-                DateTime EnrolDate = (DateTime)ResultSet["enroldate"];
+                int StudentId = Convert.ToInt32(ResultSet["studentid"]);
+                string StudentFname = ResultSet["studentfname"].ToString();
+                string StudentLname = ResultSet["studentlname"].ToString();
+                string StudentNumber = ResultSet["studentnumber"].ToString();
+                DateTime EnrolDate = Convert.ToDateTime(ResultSet["enroldate"]);
 
                 Student NewStudent = new Student();
                 NewStudent.StudentId = StudentId;
@@ -84,12 +84,11 @@ namespace project1_c_.Controllers
 
             while (ResultSet.Read())
             {
-                int StudentId = (int)ResultSet["studentid"];
-                string StudentFname = (string)ResultSet["studentfname"];
-                string StudentLname = (string)ResultSet["studentlname"];
-                string StudentNumber = (string)ResultSet["studentnumber"];
-                DateTime EnrolDate = (DateTime)ResultSet["enroldate"];
-               
+                int StudentId = Convert.ToInt32(ResultSet["studentid"]);
+                string StudentFname = ResultSet["studentfname"].ToString();
+                string StudentLname = ResultSet["studentlname"].ToString();
+                string StudentNumber = ResultSet["studentnumber"].ToString();
+                DateTime EnrolDate = Convert.ToDateTime(ResultSet["enroldate"]);               
 
                 NewStudent.StudentId = StudentId;
                 NewStudent.StudentFname = StudentFname;
